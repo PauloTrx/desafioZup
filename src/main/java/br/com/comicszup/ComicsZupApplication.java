0aebed0979ea7d20742291c2a2f100b90ec34e3e
Paulo Martins Monteiro <paulotrx@ymail.com>
2021-06-25 22:17:44 -0300

    Primeiro commit

---
+++ src/main/java/br/com/comicszup/ComicsZupApplication.java
@@ -0,0 +1,13 @@
+package br.com.comicszup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComicsZupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComicsZupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/comicszup/ComicsZupApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.comicszup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComicsZupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
