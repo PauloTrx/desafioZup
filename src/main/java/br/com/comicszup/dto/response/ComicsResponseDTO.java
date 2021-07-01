4e2061ad6211037a06fd2ab854629c4d38a5c4f1
Paulo Martins Monteiro <paulotrx@ymail.com>
2021-07-01 00:28:34 -0300

    Criação do ComicsResponseDTO

---
+++ src/main/java/br/com/comicszup/dto/response/ComicsResponseDTO.java
@@ -0,0 +1,54 @@
+package br.com.comicszup.dto.response;
+
+import br.com.comicszup.entity.Comics;
+import br.com.comicszup.entity.User;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.Column;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+public class ComicsResponseDTO {
+    private String comicId;
+    private String titulo;
+    private Float preco;
+    private String autores;
+    private String ISBN;
+    private String descricao;
+
+    public ComicsResponseDTO() {
+    }
+
+    public ComicsResponseDTO(Comics comics){
+        this.comicId = comics.getComicId();
+        this.titulo = comics.getTitulo();
+        this.preco = comics.getPreco();
+        this.autores = comics.getAutores();
+        this.ISBN = comics.getISBN();
+        this.descricao = comics.getDescricao();
+    }
+
+    public String getComicId() { return comicId; }
+
+    public void setComicId(String comicId) { this.comicId = comicId; }
+
+    public String getTitulo() { return titulo; }
+
+    public void setTitulo(String titulo) { this.titulo = titulo; }
+
+    public Float getPreco() { return preco; }
+
+    public void setPreco(Float preco) { this.preco = preco; }
+
+    public String getAutores() { return autores; }
+
+    public void setAutores(String autores) { this.autores = autores; }
+
+    public String getISBN() { return ISBN; }
+
+    public void setISBN(String ISBN) { this.ISBN = ISBN; }
+
+    public String getDescricao() { return descricao; }
+
+    public void setDescricao(String descricao) { this.descricao = descricao; }
+}
